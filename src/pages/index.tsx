16cdfa4657099a4c23b442324e1c8e839108f4bd
matheusleal-code <alanwalkker.123@gmail.com>
2021-06-01 14:55:12 -0300

    stars home css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,23 +4,41 @@ import styles from './home.module.scss'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <div className={styles.stars}>
+
+      </div>
+      <div className={styles.stars2}>
+
+      </div>
       <div className={styles.options}>
-        <div className={styles.option}>
-
+        <div className={styles.box}>
+          <div className={styles.option}>
+            <img src="characters.jpg" alt="" />
+          </div>
+          <p>Characters</p>
+          <a href=""></a>
         </div>
-        <p>Characters</p>
-        <div className={styles.option}>
-
+        <div className={styles.box}>
+          <div className={styles.option}>
+            <img src="locations.jpg" alt="" />
+          </div>
+          <p>Locations</p>
+          <a href=""></a>
         </div>
-        <p>Locations</p>
-        <div className={styles.option}>
-
+        <div className={styles.box}>
+          <div className={styles.option}>
+            <img src="episodes.jpeg" alt="" />
+          </div>
+          <p>Episodes</p>
+          <a href=""></a>
         </div>
-        <p>Episodes</p>
-        <div className={styles.option}>
-
+        <div className={styles.box}>
+          <div className={styles.option}>
+            <p>?</p>
+          </div>
+          <p>About</p>
+          <a href=""></a>
         </div>
-        <p>About</p>
       </div>
     </div>
   )
